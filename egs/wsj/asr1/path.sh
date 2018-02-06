9c9dbcac777e6260c23b78500064690d32e6148f
Jahn Heymann <heymann@nt.upb.de>
2018-02-06 10:35:38 +0100

    Adds library path for warpctc Links in missing steps and utils

--- egs/wsj/asr1/path.sh
+++ egs/wsj/asr1/path.sh
@@ -1,12 +1,18 @@
 MAIN_ROOT=$PWD/../../..
 KALDI_ROOT=$MAIN_ROOT/tools/kaldi
 SPNET_ROOT=$MAIN_ROOT/src
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$MAIN_ROOT/tools/chainer_ctc/ext/build
 
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
+
+rm utils
+ln -s $KALDI_ROOT/egs/wsj/s5/utils utils
+rm steps
+ln -s $KALDI_ROOT/egs/wsj/s5/steps steps
 
 export PATH=$SPNET_ROOT/utils/:$SPNET_ROOT/bin/:$PATH
 source $MAIN_ROOT/tools/venv/bin/activate
